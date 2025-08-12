b006dad4c9548b48677e9f52eb8681906b05f44e
rahmaAlmamari <alr816125@gmail.com>
2025-08-12 15:11:39 +0400

    Adding fields to Airport class

--- Models/Airport.cs
+++ Models/Airport.cs
@@ -8,5 +8,12 @@ namespace FlightManagementSystem.Models
 {
     public class Airport
     {
+        public int AirportId { get; set; }
+        public string IATA { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Timezone { get; set; }
+
     }
 }
