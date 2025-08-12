49d499875ec66f3a74a5272717000d1fa0de0cb9
rahmaAlmamari <alr816125@gmail.com>
2025-08-12 15:37:27 +0400

    Adding fields to FlightCrew class

--- Models/FlightCrew.cs
+++ Models/FlightCrew.cs
@@ -8,5 +8,8 @@ namespace FlightManagementSystem.Models
 {
     public class FlightCrew
     {
+        public string RoleOnFlight { get; set; }
+        public int FlightId { get; set; }
+        public int CrewId { get; set; }
     }
 }
