dccd28a78749bc78773e4fc8ac9615945366c0e6
rahmaAlmamari <alr816125@gmail.com>
2025-08-12 15:15:04 +0400

    Adding fields to Aircraft class

--- Models/Aircraft.cs
+++ Models/Aircraft.cs
@@ -8,5 +8,9 @@ namespace FlightManagementSystem.Models
 {
     public class Aircraft
     {
+        public int AircraftId { get; set; }
+        public string TailNumber { get; set; }
+        public string Model { get; set; }
+        public int Capacity { get; set; }
     }
 }
