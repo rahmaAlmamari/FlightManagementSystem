ef89d924f3cc362cf1196fd05a18f44b5cc62c5b
rahmaAlmamari <alr816125@gmail.com>
2025-08-12 15:44:21 +0400

    Adding fields to Ticket class

--- Models/Ticket.cs
+++ Models/Ticket.cs
@@ -8,5 +8,9 @@ namespace FlightManagementSystem.Models
 {
     public class Ticket
     {
+        public int TicketId { get; set; }
+        public string SeatNumber { get; set; }
+        public decimal Fare { get; set; }
+        public bool CheckedIn { get; set; }
     }
 }
