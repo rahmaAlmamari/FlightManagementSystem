b98ef2b5abb688f56b3d135534a79c07b416af45
rahmaAlmamari <alr816125@gmail.com>
2025-08-12 15:21:52 +0400

    Adding fields to Booking class

--- Models/Booking.cs
+++ Models/Booking.cs
@@ -8,5 +8,9 @@ namespace FlightManagementSystem.Models
 {
     public class Booking
     {
+        public int BookingId { get; set; }
+        public string BookingRef { get; set; }
+        public DateTime BookingDate { get; set; }
+        public string Status { get; set; } 
     }
 }
