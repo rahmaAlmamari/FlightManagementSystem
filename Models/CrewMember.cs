40490246864de6f9e1d77eb9144a5b3148df444e
rahmaAlmamari <alr816125@gmail.com>
2025-08-12 15:24:26 +0400

    Adding fields to CrewMember class

--- Models/CrewMember.cs
+++ Models/CrewMember.cs
@@ -8,5 +8,9 @@ namespace FlightManagementSystem.Models
 {
     public class CrewMember
     {
+        public int CrewId { get; set; }
+        public string FullName { get; set; }
+        public string Role { get; set; } 
+        public string LicenseNo { get; set; }
     }
 }
