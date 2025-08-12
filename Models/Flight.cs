9b39f654e9e8cfcd3ec17f05f5169d72868ccb38
rahmaAlmamari <alr816125@gmail.com>
2025-08-12 15:32:07 +0400

    Adding fields to Flight class

--- Models/Flight.cs
+++ Models/Flight.cs
@@ -8,5 +8,10 @@ namespace FlightManagementSystem.Models
 {
     public class Flight
     {
+        public int FlightId { get; set; }
+        public string FlightNumber { get; set; }
+        public DateTime DepartureUtc { get; set; }
+        public DateTime ArrivalUtc { get; set; }
+        public string Status { get; set; } // e.g., Scheduled, Delayed, Cancelled
     }
 }
