846c065f3c58a38f15969defd79190547f055eb7
rahmaAlmamari <alr816125@gmail.com>
2025-08-14 15:19:32 +0400

    Set AirportRepository class to implement IAirportRepository

--- Repostories/AirportRepository.cs
+++ Repostories/AirportRepository.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace FlightManagementSystem.Repostories
 {
-    public class AirportRepository
+    public class AirportRepository : IAirportRepository
     {
         //to create field of FlightDbContext ...
         private readonly FlightDbContext _context;
