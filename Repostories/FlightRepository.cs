f3e623fde2cc96c3461ed8cb1eafaa35dd9ddb33
rahmaAlmamari <alr816125@gmail.com>
2025-08-14 16:06:07 +0400

    Set FlightRepository class to implement IFlightRepository

--- Repostories/FlightRepository.cs
+++ Repostories/FlightRepository.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace FlightManagementSystem.Repostories
 {
-    public class FlightRepository
+    public class FlightRepository : IFlightRepository
     {
         //to create field of FlightDbContext ...
         private readonly FlightDbContext _context;
