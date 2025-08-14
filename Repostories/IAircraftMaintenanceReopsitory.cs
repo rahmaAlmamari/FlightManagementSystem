e5bf4a3409270baa07ab03c276d973286a6448d3
rahmaAlmamari <alr816125@gmail.com>
2025-08-14 15:08:00 +0400

    Extract IAircraftMaintenanceReopsitory interface

---
+++ Repostories/IAircraftMaintenanceReopsitory.cs
@@ -0,0 +1,13 @@
+﻿using FlightManagementSystem.Models;
+
+namespace FlightManagementSystem.Repostories
+{
+    public interface IAircraftMaintenanceReopsitory
+    {
+        void AddAircraftMaintenance(AircraftMaintenance maintenance);
+        void DeleteAircraftMaintenance(int id);
+        AircraftMaintenance GetAircraftMaintenanceById(int id);
+        IEnumerable<AircraftMaintenance> GetAllAircraftMaintenances();
+        void UpdateAircraftMaintenance(AircraftMaintenance maintenance);
+    }
+}
