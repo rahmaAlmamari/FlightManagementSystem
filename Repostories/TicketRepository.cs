2abfb5090cbaf080e43015b427525a6be11e6be3
rahmaAlmamari <alr816125@gmail.com>
2025-08-14 16:12:30 +0400

    to create field of FlightDbContext TicketRepository

--- Repostories/TicketRepository.cs
+++ Repostories/TicketRepository.cs
@@ -8,5 +8,7 @@ namespace FlightManagementSystem.Repostories
 {
     public class TicketRepository
     {
+        //to create field of FlightDbContext ...
+        private readonly FlightDbContext _context;
     }
 }
