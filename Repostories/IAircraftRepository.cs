f79d0eb17ed80131b36b22dfc3eb49dd2f0439d3
rahmaAlmamari <alr816125@gmail.com>
2025-08-14 15:12:40 +0400

    Extract IAircraftRepository interface

---
+++ Repostories/IAircraftRepository.cs
@@ -0,0 +1,13 @@
+﻿using FlightManagementSystem.Models;
+
+namespace FlightManagementSystem.Repostories
+{
+    public interface IAircraftRepository
+    {
+        void AddAircraft(Aircraft aircraft);
+        void DeleteAircraft(int id);
+        Aircraft GetAircraftById(int id);
+        IEnumerable<Aircraft> GetAllAircrafts();
+        void UpdateAircraft(Aircraft aircraft);
+    }
+}
