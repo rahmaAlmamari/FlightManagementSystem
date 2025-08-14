ec4a60f45c2fe0af536d7a154bccf1a49ef5ead9
rahmaAlmamari <alr816125@gmail.com>
2025-08-14 15:35:24 +0400

    Extract IBookingRepository interface

---
+++ Repostories/IBookingRepository.cs
@@ -0,0 +1,13 @@
+﻿using FlightManagementSystem.Models;
+
+namespace FlightManagementSystem.Repostories
+{
+    public interface IBookingRepository
+    {
+        void AddBooking(Booking booking);
+        void DeleteBooking(int id);
+        IEnumerable<Booking> GetAllBookings();
+        Booking GetBookingById(int id);
+        void UpdateBooking(Booking booking);
+    }
+}
