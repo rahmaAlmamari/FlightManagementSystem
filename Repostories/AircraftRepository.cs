ca825cdc629bad25c3a4bb7954ae5851ae32827c
rahmaAlmamari <alr816125@gmail.com>
2025-08-14 15:10:17 +0400

    to get all aircrafts

--- Repostories/AircraftRepository.cs
+++ Repostories/AircraftRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FlightManagementSystem.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,6 +15,11 @@ namespace FlightManagementSystem.Repostories
         public AircraftRepository(FlightDbContext context)
         {
             _context = context;
+        }
+        //to get all aircrafts ...
+        public IEnumerable<Aircraft> GetAllAircrafts()
+        {
+            return _context.Aircrafts.ToList();
         }
     }
 }
