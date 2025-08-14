a9442c8c3ff9a8457c7fd4fcce60b988719bd22c
rahmaAlmamari <alr816125@gmail.com>
2025-08-14 16:02:16 +0400

    Extract IFlightCrewRepository interface

---
+++ Repostories/IFlightCrewRepository.cs
@@ -0,0 +1,13 @@
+﻿using FlightManagementSystem.Models;
+
+namespace FlightManagementSystem.Repostories
+{
+    public interface IFlightCrewRepository
+    {
+        void AddFlightCrewMember(FlightCrew flightCrew);
+        void DeleteFlightCrewMember(int id);
+        IEnumerable<FlightCrew> GetAllFlightCrewMembers();
+        FlightCrew GetFlightCrewMemberById(int id);
+        void UpdateFlightCrewMember(FlightCrew flightCrew);
+    }
+}
