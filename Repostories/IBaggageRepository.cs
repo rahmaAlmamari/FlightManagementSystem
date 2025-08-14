991f20d0fbe71b18e57295d63970310e9d8cdef8
rahmaAlmamari <alr816125@gmail.com>
2025-08-14 15:28:49 +0400

    Extract IBaggageRepository interface

---
+++ Repostories/IBaggageRepository.cs
@@ -0,0 +1,13 @@
+﻿using FlightManagementSystem.Models;
+
+namespace FlightManagementSystem.Repostories
+{
+    public interface IBaggageRepository
+    {
+        void AddBaggage(Baggage baggage);
+        void DeleteBaggage(int id);
+        IEnumerable<Baggage> GetAllBaggage();
+        Baggage GetBaggageById(int id);
+        void UpdateBaggage(Baggage baggage);
+    }
+}
