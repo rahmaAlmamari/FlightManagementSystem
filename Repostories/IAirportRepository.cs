a69d9781dc6c4e8ec9df9a69f1521401fe8579f5
rahmaAlmamari <alr816125@gmail.com>
2025-08-14 15:18:40 +0400

    Extract IAirportRepository interface

---
+++ Repostories/IAirportRepository.cs
@@ -0,0 +1,13 @@
+﻿using FlightManagementSystem.Models;
+
+namespace FlightManagementSystem.Repostories
+{
+    public interface IAirportRepository
+    {
+        void AddAirport(Airport airport);
+        void DeleteAirport(int id);
+        Airport GetAirportById(int id);
+        IEnumerable<Airport> GetAllAirports();
+        void UpdateAirport(Airport airport);
+    }
+}
