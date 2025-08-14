34947c6558e9b0820654b56683d128381283d50b
rahmaAlmamari <alr816125@gmail.com>
2025-08-14 10:59:22 +0400

    Adding OnConfiguring method to FlightDbContext file

--- FlightDbContext.cs
+++ FlightDbContext.cs
@@ -5,9 +5,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace FlightManagementSystem
 {
     public class FlightDbContext : DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-OGTF9QH;Initial Catalog=StoreDB;Integrated Security=True;TrustServerCertificate=True");
+        }
+
     }
 }
