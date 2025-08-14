699d33ff67f415c913013f9a7c0451cffc145d4e
rahmaAlmamari <alr816125@gmail.com>
2025-08-14 16:05:42 +0400

    Extract IFlightRepository interface

---
+++ Repostories/IFlightRepository.cs
@@ -0,0 +1,13 @@
+﻿using FlightManagementSystem.Models;
+
+namespace FlightManagementSystem.Repostories
+{
+    public interface IFlightRepository
+    {
+        void AddFlight(Flight flight);
+        void DeleteFlight(int id);
+        IEnumerable<Flight> GetAllFlights();
+        Flight GetFlightById(int id);
+        void UpdateFlight(Flight flight);
+    }
+}
