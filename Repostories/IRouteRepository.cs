5cf6ad0e114b6d4fd024c19f5970fbf5461c28c8
rahmaAlmamari <alr816125@gmail.com>
2025-08-14 16:11:42 +0400

    Extract IRouteRepository interface

---
+++ Repostories/IRouteRepository.cs
@@ -0,0 +1,13 @@
+﻿using FlightManagementSystem.Models;
+
+namespace FlightManagementSystem.Repostories
+{
+    public interface IRouteRepository
+    {
+        void AddRoute(Route route);
+        void DeleteRoute(int id);
+        IEnumerable<Route> GetAllRoutes();
+        Route GetRouteById(int id);
+        void UpdateRoute(Route route);
+    }
+}
