2377a1bc916ce30820c5d71680a331a42063fe3c
rahmaAlmamari <alr816125@gmail.com>
2025-08-14 11:14:39 +0400

    Adding DbSet for all models into FlightDbContext file

--- FlightDbContext.cs
+++ FlightDbContext.cs
@@ -14,6 +14,19 @@ namespace FlightManagementSystem
         {
             optionsBuilder.UseSqlServer("Data Source=DESKTOP-OGTF9QH;Initial Catalog=StoreDB;Integrated Security=True;TrustServerCertificate=True");
         }
+        // DbSet properties for each model ...
+        public DbSet<Aircraft> Aircrafts { get; set; }
+        public DbSet<AircraftMaintenance> AircraftMaintenances { get; set; }
+        public DbSet<Airport> Airports { get; set; }
+        public DbSet<Baggage> Baggages { get; set; }
+        public DbSet<Flight> Flights { get; set; }
+        public DbSet<Ticket> Tickets { get; set; }
+        public DbSet<Passenger> Passengers { get; set; }
+        public DbSet<FlightCrew> FlightCrews { get; set; }
+        public DbSet<Route> Routes { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<CrewMember> CrewMembers { get; set; }
+
 
     }
 }
