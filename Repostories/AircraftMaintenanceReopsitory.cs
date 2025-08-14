66818a49e24a9b1b46b7fdf66d4482b12e819a52
rahmaAlmamari <alr816125@gmail.com>
2025-08-14 15:05:45 +0400

    GetAllAircraftMaintenances

--- Repostories/AircraftMaintenanceReopsitory.cs
+++ Repostories/AircraftMaintenanceReopsitory.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FlightManagementSystem.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,6 +15,12 @@ namespace FlightManagementSystem.Repostories
         public AircraftMaintenanceReopsitory(FlightDbContext context)
         {
             _context = context;
+        }
+        //to get all aircraft maintenances ...
+        public IEnumerable<AircraftMaintenance> GetAllAircraftMaintenances()
+        {
+            return _context.AircraftMaintenances.ToList();
+
         }
     }
 }
