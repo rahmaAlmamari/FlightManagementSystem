cea3d7bf147b4ba124b1703b3367b846de7574b5
rahmaAlmamari <alr816125@gmail.com>
2025-08-14 16:14:32 +0400

    Extract ITicketRepository interface

---
+++ Repostories/ITicketRepository.cs
@@ -0,0 +1,13 @@
+﻿using FlightManagementSystem.Models;
+
+namespace FlightManagementSystem.Repostories
+{
+    public interface ITicketRepository
+    {
+        void AddTicket(Ticket ticket);
+        void DeleteTicket(int id);
+        IEnumerable<Ticket> GetAllTickets();
+        Ticket GetTicketById(int id);
+        void UpdateTicket(Ticket ticket);
+    }
+}
