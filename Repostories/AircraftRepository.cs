b90da3285f492e7d0f32f7415df402df743b073e
rahmaAlmamari <alr816125@gmail.com>
2025-08-14 15:09:20 +0400

    to create field of FlightDbContext

--- Repostories/AircraftRepository.cs
+++ Repostories/AircraftRepository.cs
@@ -8,5 +8,7 @@ namespace FlightManagementSystem.Repostories
 {
     public class AircraftRepository
     {
+        //to create field of FlightDbContext ...
+        private readonly FlightDbContext _context;
     }
 }
