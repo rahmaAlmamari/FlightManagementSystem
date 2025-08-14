c4c33eb5563d7f262cc750b651c83cfd31873a74
rahmaAlmamari <alr816125@gmail.com>
2025-08-14 16:08:34 +0400

    Extract IPassengerRepository interface

---
+++ Repostories/IPassengerRepository.cs
@@ -0,0 +1,13 @@
+﻿using FlightManagementSystem.Models;
+
+namespace FlightManagementSystem.Repostories
+{
+    public interface IPassengerRepository
+    {
+        void AddPassenger(Passenger passenger);
+        void DeletePassenger(int id);
+        IEnumerable<Passenger> GetAllPassengers();
+        Passenger GetPassengerById(int id);
+        void UpdatePassenger(Passenger passenger);
+    }
+}
