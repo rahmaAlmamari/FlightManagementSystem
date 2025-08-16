e6b2d7594020ced7e6f924a9bdb8b300f31dd22b
rahmaAlmamari <alr816125@gmail.com>
2025-08-16 10:12:16 +0400

    to run the CreateSampleData method from FlightSeedData class

--- Program.cs
+++ Program.cs
@@ -1,10 +1,32 @@
-﻿namespace FlightManagementSystem
+﻿using FlightManagementSystem.Data;
+using FlightManagementSystem.Repostories;
+
+namespace FlightManagementSystem
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            //to creat object for FlightDbContext ...
+            FlightDbContext context = new FlightDbContext();
+            //to create object for repositories ...
+            IAirportRepository airportRepo = new AirportRepository(context);
+            IRouteRepository routeRepo = new RouteRepository(context);
+            IAircraftRepository aircraftRepo = new AircraftRepository(context);
+            IFlightRepository flightRepo = new FlightRepository(context);
+            IPassengerRepository passengerRepo = new PassengerRepository(context);
+            IBookingRepository bookingRepo = new BookingRepository(context);
+            ITicketRepository ticketRepo = new TicketRepository(context);
+            IBaggageRepository baggageRepo = new BaggageRepository(context);
+            ICrewMemberRepository crewRepo = new CrewMemberRepository(context);
+            IFlightCrewRepository flightCrewRepo = new FlightCrewRepository(context);
+            IAircraftMaintenanceReopsitory maintenanceRepo = new AircraftMaintenanceReopsitory(context);
 
+            ////to run the CreateSampleData method from FlightSeedData class ...
+            ////to call the CreateSampleData method ...
+            //FlightSeedData.CreateSampleData(airportRepo, routeRepo, aircraftRepo, flightRepo, 
+            //                                passengerRepo, bookingRepo, ticketRepo, baggageRepo, 
+            //                                crewRepo, flightCrewRepo, maintenanceRepo);
         }
     }
 }
