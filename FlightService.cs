88626518011cd095e38f0142abb327edd79d88ee
rahmaAlmamari <alr816125@gmail.com>
2025-08-16 18:03:59 +0400

    modify logic of GetDailyFlightManifest

--- FlightService.cs
+++ FlightService.cs
@@ -67,26 +67,74 @@ namespace FlightManagementSystem
                 DateTime ArrivalUtc = flight.ArrivalUtc;
                 //to get flight route by flight.RouteId ...
                 Route FlightRoute = _routeRepository.GetRouteById(flight.RouteId);
-                //to get OriginIATA and DestIATA from FlightRoute ...
-                string OriginIATA = FlightRoute.Origin.IATA;
+                //to get OriginIATA from FlightRoute ...
+                // string OriginIATA = FlightRoute.Origin.IATA;
+                //to get airport by FlightRoute.AirportIdOrigin ...
+                Airport OriginAirport = _airportRepository.GetAirportById(FlightRoute.AirportIdOrigin);
+                string OriginIATA = OriginAirport.IATA;
                 //to get DestIATA from FlightRoute ...
-                string DestIATA = FlightRoute.Destination.IATA;
+                //string DestIATA = FlightRoute.Destination.IATA;
+                //to get airport by FlightRoute.AirportIdDestination ...
+                Airport DestAirport = _airportRepository.GetAirportById(FlightRoute.AirportIdDestination);
+                string DestIATA = DestAirport.IATA;
                 //to get AircraftTail by flight.AircraftId ...
                 Aircraft FlightAircraft = _aircraftRepository.GetAircraftById(flight.AircraftId);
                 string AircraftTail = FlightAircraft.TailNumber;
                 //to get PassengerCount by flight.Tickets.Count ...
-                int PassengerCount = flight.Tickets.Count;
+                //int PassengerCount = flight.Tickets.Count;
+                int PassengerCount = _ticketRepository.GetAllTickets()
+                    .Where(ticket => ticket.FlightId == flight.FlightId)
+                    .Count();
                 //to get TotalBaggageWeight ...
-                decimal TotalBaggageWeight = flight.Tickets
-                    .SelectMany(ticket => ticket.Baggage)
+                //decimal TotalBaggageWeight = flight.Tickets
+                //    .SelectMany(ticket => ticket.Baggage)
+                //    .Sum(baggage => baggage.WeightKg);
+                decimal TotalBaggageWeight = _baggageRepository.GetAllBaggage()
+                    .Where(baggage => baggage.Ticket.FlightId == flight.FlightId)
                     .Sum(baggage => baggage.WeightKg);
                 //to get CrewList by flight.FlightCrewMembers ...
-                List<DailyFlightManifestOutput_CrewList> CrewList = flight.FlightCrewMembers
-                    .Select(crew => new DailyFlightManifestOutput_CrewList
+                //List<DailyFlightManifestOutput_CrewList> CrewList = flight.FlightCrewMembers
+                //    .Select(crew => new DailyFlightManifestOutput_CrewList
+                //    {
+                //        Name = crew.CrewMember.FullName,
+                //        Role = crew.RoleOnFlight
+                //    }).ToList();
+                var CrewList = _flightCrewRepository.GetAllFlightCrewMembers()
+                    .Where(flightCrew => flightCrew.FlightId == flight.FlightId)
+                    .ToList();
+                List<DailyFlightManifestOutput_CrewList> CrewListOutput = new List<DailyFlightManifestOutput_CrewList>();
+                foreach (var crew in CrewList)
+                {
+                    if (crew.CrewMember == null)
                     {
-                        Name = crew.CrewMember.FullName,
-                        Role = crew.RoleOnFlight
-                    }).ToList();
+                        CrewListOutput.Add(new DailyFlightManifestOutput_CrewList
+                        {
+                            Name = "null",
+                            Role = "null"
+                        });
+                    }
+                    else
+                    {
+                        CrewListOutput.Add(new DailyFlightManifestOutput_CrewList
+                        {
+                            Name = crew.CrewMember.FullName,
+                            Role = crew.RoleOnFlight
+                        });
+                    }
+                }
+                //to create a new DailyFlightManifestOutput object and add it to the manifest list ...
+                manifest.Add(new DailyFlightManifestOutput
+                {
+                    FlightNumber = FilghtNumber,
+                    DepartureUtc = DepartureUtc,
+                    ArrivalUtc = ArrivalUtc,
+                    OriginIATA = OriginIATA,
+                    DestIATA = DestIATA,
+                    AircraftTail = AircraftTail,
+                    PassengerCount = PassengerCount,
+                    TotalBaggageWeight = TotalBaggageWeight,
+                    CrewList = CrewListOutput
+                });
             }
             return manifest;
         }
