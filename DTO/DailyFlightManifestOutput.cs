7e6a395f7a33f578292916837b7d6d2d24730dea
rahmaAlmamari <alr816125@gmail.com>
2025-08-16 13:01:14 +0400

    Adding fields to DailyFlightManifestOutput class

--- DTO/DailyFlightManifestOutput.cs
+++ DTO/DailyFlightManifestOutput.cs
@@ -8,5 +8,15 @@ namespace FlightManagementSystem.DTO
 {
     public class DailyFlightManifestOutput
     {
+        public string FlightNumber { get; set; }
+        public DateTime DepartureUtc { get; set; }
+        public DateTime ArrivalUtc { get; set; }
+        public string OriginIATA { get; set; }
+        public string DestIATA { get; set; }
+        public string AircraftTail { get; set; }
+        public int PassengerCount { get; set; }
+        public decimal TotalBaggageWeight { get; set; }
+        public List<DailyFlightManifestOutput_CrewList> CrewList { get; set; }
+
     }
 }
