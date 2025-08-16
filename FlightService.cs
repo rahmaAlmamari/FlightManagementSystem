fd6417758cbdef81c3b96bf8987e3ebbf4e1516a
rahmaAlmamari <alr816125@gmail.com>
2025-08-16 12:47:25 +0400

    to create a private readonly field for each repository

--- FlightService.cs
+++ FlightService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FlightManagementSystem.Repostories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,19 @@ namespace FlightManagementSystem
 {
     public class FlightService
     {
-        //to creat object for FlightDbContext ...
-        FlightDbContext context = new FlightDbContext();
+        //to create a private readonly field for each repository ...
+        private readonly IAircraftMaintenanceReopsitory _AircraftMaintenanceReopsitory;
+        private readonly IAirportRepository _airportRepository;
+        private readonly IRouteRepository _routeRepository;
+        private readonly IAircraftRepository _aircraftRepository;
+        private readonly IFlightRepository _flightRepository;
+        private readonly IPassengerRepository _passengerRepository;
+        private readonly IBookingRepository _bookingRepository;
+        private readonly ITicketRepository _ticketRepository;
+        private readonly IBaggageRepository _baggageRepository;
+        private readonly ICrewMemberRepository _crewMemberRepository;
+        private readonly IFlightCrewRepository _flightCrewRepository;
+
+
     }
 }
