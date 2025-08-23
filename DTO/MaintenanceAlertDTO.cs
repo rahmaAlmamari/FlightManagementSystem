8930097e30a8cd6d7ea96effc3d827a00e222814
rahmaAlmamari <alr816125@gmail.com>
2025-08-23 10:29:46 +0400

    Adding MaintenanceAlertDTO DTO

---
+++ DTO/MaintenanceAlertDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace FlightManagementSystem.DTOs
+{
+    public class MaintenanceAlertDTO
+    {
+        public int AircraftId { get; set; }
+        public string AircraftModel { get; set; }
+        public double TotalFlightHours { get; set; }
+        public DateTime LastMaintenanceDate { get; set; }
+        public string Reason { get; set; } // e.g. "Exceeded Hours" or "Maintenance Overdue"
+    }
+}
