5414d654084e030c676cb8febce216c447d0a36b
rahmaAlmamari <alr816125@gmail.com>
2025-08-23 10:13:35 +0400

    Adding FrequentFlierDTO

---
+++ DTO/FrequentFlierDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace FlightManagementSystem.DTOs
+{
+    public class FrequentFlierDTO
+    {
+        public string PassengerName { get; set; }
+        public int TotalFlights { get; set; }
+        public int TotalDistanceKm { get; set; }
+    }
+}
