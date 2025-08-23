c4f171f996724cb3f283a7c57a472303803ec424
rahmaAlmamari <alr816125@gmail.com>
2025-08-23 09:52:14 +0400

    Adding ConnectionItineraryDTO DTO

---
+++ DTO/ConnectionItineraryDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightManagementSystem.DTO
+{
+    class ConnectionItineraryDTO
+    {
+        public string PassengerName { get; set; }
+        public string BookingRef { get; set; }
+        public List<FlightSegmentDTO> Segments { get; set; }
+    }
+}
