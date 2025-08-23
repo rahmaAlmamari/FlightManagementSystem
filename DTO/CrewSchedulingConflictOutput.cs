1a3f78cc710127efd2ebf872588c87d6cb693821
rahmaAlmamari <alr816125@gmail.com>
2025-08-23 09:21:05 +0400

    Adding CrewSchedulingConflictOutput DTO

---
+++ DTO/CrewSchedulingConflictOutput.cs
@@ -0,0 +1,17 @@
+﻿using FlightManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightManagementSystem.DTO
+{
+    public class CrewSchedulingConflictOutput
+    {
+        public int CrewId { get; set; }
+        public string CrewName { get; set; }
+        public string FlightANumber { get; set; }
+        public string FlightBNumber { get; set; }
+    }
+}
