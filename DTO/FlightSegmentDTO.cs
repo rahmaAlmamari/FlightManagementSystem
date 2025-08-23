add6d33de3f151b6b833759c287817f3688468d7
rahmaAlmamari <alr816125@gmail.com>
2025-08-23 09:51:56 +0400

    Adding FlightSegmentDTO DTO

---
+++ DTO/FlightSegmentDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightManagementSystem.DTO
+{
+    class FlightSegmentDTO
+    {
+        public string FlightNumber { get; set; }
+        public string OriginIATA { get; set; }
+        public string DestinationIATA { get; set; }
+        public DateTime DepartureUtc { get; set; }
+        public DateTime ArrivalUtc { get; set; }
+    }
+}
