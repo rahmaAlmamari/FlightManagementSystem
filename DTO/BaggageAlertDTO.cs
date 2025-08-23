1822e4f35fe09914905c2cd59a0cf15411964504
rahmaAlmamari <alr816125@gmail.com>
2025-08-23 10:45:01 +0400

    Adding BaggageAlertDTO DTO

---
+++ DTO/BaggageAlertDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightManagementSystem.DTO
+{
+    public class BaggageAlertDTO
+    {
+        public int TicketId { get; set; }
+        public int PassengerId { get; set; }
+        public string PassengerName { get; set; }
+        public decimal TotalBaggageWeight { get; set; }
+        public decimal Threshold { get; set; }
+    }
+
+}
